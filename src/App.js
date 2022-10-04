732f8c91fdced1f109481e69e5cf3c979906f94d
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-04 15:39:12 -0300

    se hicieron las rutas para cada una de las paginas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Contest from './pages/Contest'
+import Products from './pages/Products'
 
 
 function App() {
   return (
-    <div >
-      <Home/>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/contest' element={<Contest />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
