35d9dbdbf2196f24ef6983112c82e5760a906c33
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 18:48:38 -0300

    se ponen el layout en las paginas correspondiente y no en el componente

--- src/components/products/Tshirt.jsx
+++ src/components/products/Tshirt.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Layout from '../../layout/Layout'
 
 export default function Tshirt() {
   return (
     <>
-        <Layout>
-            Tshirt
-        </Layout>
+        Tshirt
     </>
   )
 }
--- src/pages/Hoodie.js
+++ src/pages/Hoodie.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ProductHoodie from '../components/products/Hoodie'
+import Layout from '../layout/Layout'
 
 export default function Hoodie() {
   return (
     <>
+    <Layout>
         <ProductHoodie />
+    </Layout>
     </>
   )
 }
