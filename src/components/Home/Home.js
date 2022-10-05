bce7bf8c5117bcbd3f2e124403bb89465722ba68
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 13:58:21 -0300

    destacados

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useGetHoodieQuery } from '../../features/hoodieAPI'
 
 const Home = () => {
+
+
+    let params = ''
+    const { 
+        data: hoodie,
+        isLoading,
+        isSuccess,
+        isError
+     } = useGetHoodieQuery(params)
+
+
+    filter = []
+    if(isLoading){
+        filter = []
+    }else if(isSuccess){
+        filter = hoodie.response
+        if (filter === []) {
+            filter = []
+        }
+    }
+
     return (
         <div>Inicio</div>
     )
