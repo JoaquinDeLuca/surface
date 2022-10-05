3af3979683428d0bb9f65bacc52c91f710ea5520
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 16:08:16 -0300

    se agrega componente de productos destacados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../layout/Layout'
+import ProductosDestacos from '../components/Home/ProductosDestacos'
 
 const Home = () => {
     return (
         <Layout>
-            <div>Inicio</div>
+            <ProductosDestacos />
         </Layout>
     )
 }
