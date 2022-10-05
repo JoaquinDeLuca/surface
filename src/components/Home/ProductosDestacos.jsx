2daa82cda942697d5109e50f679cfba9b8b74c39
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 16:06:49 -0300

    se pintan los productos destacados

---
+++ src/components/Home/ProductosDestacos.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useGetHoodieQuery } from '../../features/hoodieAPI'
+import PrintProduct from './PrintProduct'
+
+export default function ProductosDestacos() {
+    let params = ''
+    const { 
+        data: hoodie,
+        isLoading, 
+        isSuccess,
+        isError
+    } = useGetHoodieQuery(params)
+
+    let data = [];
+
+    if(isLoading){
+        data = []
+    }
+    if(isSuccess){
+        data = hoodie.response
+    }
+    console.log(hoodie)
+
+    return (
+        <>
+            <h2 className='h3'>Productos Destacados</h2>
+            <div className='container'>
+                {data.slice(1,5).map( (hoodies, index )=> <PrintProduct className={index %2 === 0 ? 'normal' : 'reverse'} key={hoodies.price} data={hoodies} /> ) }
+            </div>
+        </>
+    )
+}
