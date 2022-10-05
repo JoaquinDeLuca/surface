08bc7688f5a79ffe45d4b74ea427043cf1016370
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 18:36:39 -0300

    pagina de buzos

---
+++ src/pages/Hoodie.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductHoodie from '../components/products/Hoodie'
+
+export default function Hoodie() {
+  return (
+    <>
+        <ProductHoodie />
+    </>
+  )
+}
