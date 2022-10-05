7aeed5ec21b76c1db07cda73e2dd5008d06f2830
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 19:46:53 -0300

    card de Hoodie

--- src/components/products/Hoodie.jsx
+++ src/components/products/Hoodie.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-import Layout from '../../layout/Layout'
+import { useGetHoodieQuery } from '../../features/hoodieAPI'
 
 export default function Hoodie() {
-  return (
+    let params = ''
+    const { 
+        data: hoodie,
+        isLoading, 
+        isSuccess,
+        isError
+    } = useGetHoodieQuery(params)
 
-    <>
-        Hoodie
-    </>
-  )
+    let data = [];
+
+    if(isLoading){
+        data = []
+    }
+    if(isSuccess){
+        data = hoodie.response
+    }
+    console.log(hoodie)
+
+
+    const PrintHoodie = (hoodie) => {
+        return (
+            <div key={hoodie._id} className='cardProduct'>
+              <img src={hoodie.photo} alt='Hoodie'/>
+              <div className='cardBody'>
+                <h3>X nombre</h3>
+                <p>Descripcion: ${hoodie.decription}</p>
+                <p>Precio: ${hoodie.price}</p>
+                <p>Stock: {hoodie.stock}</p>
+                <div className='addCart'>
+                  <div>Futuro select</div>
+                  <div>Añadir al carrito</div>
+                </div>
+              </div>
+            </div>
+          )
+    }
+
+    return (
+        <>
+            {data.map(PrintHoodie)}
+        </>
+    )
 }
