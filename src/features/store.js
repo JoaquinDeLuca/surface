ab79c5a207c51f7fb3eee82ec5e3580775e87b5c
laza33salas <lazarocode@gmail.com>
2022-10-04 21:18:40 -0300

    import apis user and cap

--- src/features/store.js
+++ src/features/store.js
@@ -1,17 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import hoodie from './hoodieAPI'
+import userApi from './userAPI'
+import capApi from './capAPI'
 
 
-
-const store = configureStore({ 
+const store = configureStore({
     reducer: {
 
-        [hoodie.reducerPath]: hoodie.reducer
+        [hoodie.reducerPath]: hoodie.reducer,
+        [userApi.reducerPath]: userApi.reducer,
+        [capApi.reducerPath]: capApi.reducer,
+
     },
 
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-        .concat(hoodie.middleware)
+        getDefaultMiddleware()
+            .concat(hoodie.middleware, capApi.middleware, userApi.middleware)
 })
 
 export default store
