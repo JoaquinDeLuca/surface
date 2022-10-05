b40543d92164e1d3a1ff29e4519351e823051c3c
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 18:37:49 -0300

    componente que pinta los buzos

---
+++ src/components/products/Hoodie.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../layout/Layout'
+
+export default function Hoodie() {
+  return (
+
+    <>
+        <Layout>
+            Hoodie
+        </Layout>
+    </>
+  )
+}
