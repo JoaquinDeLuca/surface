2a7f009382f38fb57a1f7f9d8bdca0f7b8794684
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-05 22:00:42 -0300

    componente que pinta las card

--- src/components/products/Hoodie.jsx
+++ src/components/products/Hoodie.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { useGetHoodieQuery } from '../../features/hoodieAPI';
+import './product.css'
+import { Link, useParams } from 'react-router-dom';
 
 export default function Hoodie() {
+
     let params = ''
     const { 
         data: hoodie,
@@ -18,30 +21,34 @@ export default function Hoodie() {
     if(isSuccess){
         data = hoodie.response
     }
-    console.log(hoodie)
+    // console.log(hoodie)
 
 
     const PrintHoodie = (hoodie) => {
-        return (
+        return(
             <div key={hoodie._id} className='cardProduct'>
-              <img src={hoodie.photo} alt='Hoodie'/>
+              <div className='imgContainer'>
+                <img src={hoodie.photo} alt='tshirt'/>
+              </div>
               <div className='cardBody'>
-                <h3>X nombre</h3>
-                <p>Descripcion: ${hoodie.decription}</p>
-                <p>Precio: ${hoodie.price}</p>
-                <p>Stock: {hoodie.stock}</p>
+                <h3>{hoodie.name}</h3>
+                <p className='p'>Descripcion: {hoodie.description}</p>
+                <div className='cardData'>
+                  <p>Stock: {hoodie.stock}</p>
+                  <p>Precio: ${hoodie.price}</p>
+                </div>
                 <div className='addCart'>
                   <div>Futuro select</div>
-                  <div>Añadir al carrito</div>
+                  <Link style={{ textDecoration: "none"}}  to={`/shoppingcart/${hoodie._id}`}><div className='buttonAddCart'>Añadir al carrito</div></Link>
                 </div>
               </div>
             </div>
-          )
+        )
     }
 
     return (
-        <>
+        <div className="container">
             {data.map(PrintHoodie)}
-        </>
+        </div>
     )
 }
