8b3d57cbf731f0fe9f58d4ff33269f376c01887a
laza33salas <lazarocode@gmail.com>
2022-10-07 14:24:07 -0300

    add totalCount

--- src/components/header/User/User.jsx
+++ src/components/header/User/User.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {useState} from 'react'
 import './user.css'
 import { AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 const User = () => {
@@ -16,10 +18,13 @@ const User = () => {
     }
   }
 
+const {totalCount} = useSelector(state => state.cart)
+
   return (
     <>
       <div className='userContent-img'>
-        <AiOutlineShoppingCart size='28px' className='cart'/>
+        {totalCount}
+        <Link to='/cart' style={{ color: "white"}}><AiOutlineShoppingCart  size='28px' className='cart'/></Link>
         <AiOutlineUser size='28px'  onClick={changeDisplay}  style={{width:'54px', borderRadius:'100%', cursor:'pointer'}} />
         {/* <img onClick={changeDisplay} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc3YubX90VNZaT4guYBviMOr2y1J0E9_YEGzcFJg-ucQ&s' style={{width:'54px', borderRadius:'100%', cursor:'pointer'}} alt='user profile pic'/> */}
       </div>
