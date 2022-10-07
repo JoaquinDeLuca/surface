420668ffc604fd4705163039e61690733a9a16bb
laza33salas <lazarocode@gmail.com>
2022-10-07 14:22:06 -0300

    create page cart

---
+++ src/pages/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CartComponent from '../components/Cart/CartComponent'
+import Layout from '../layout/Layout'
+
+const Cart = () => {
+    return (
+
+        <Layout>
+            <CartComponent />
+        </Layout>
+
+    )
+}
+
+export default Cart
