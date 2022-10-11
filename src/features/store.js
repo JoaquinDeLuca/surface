006af2a949c0d0c07c4bf17340dc9fb8ccf443b9
JoaquinDeLuca <joaqui222@gmail.com>
2022-10-11 16:51:51 -0300

    se corrige el error

--- src/features/store.js
+++ src/features/store.js
@@ -29,7 +29,7 @@ const store = configureStore({
         [tShirt.reducerPath]: tShirt.reducer,
         [authApi.reducerPath]: authApi.reducer,
 
-        user: UserStatus
+        user: UserStatus,
         cart: persistedReducer
 
     },
