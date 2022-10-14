e5f95eb893d489dc62034062a8be9c92b443f30b
adrieldussex <adrieldussex@gmail.com>
2022-10-13 22:36:57 -0300

    Modifiend Product

--- src/components/Home/PrintProduct.jsx
+++ src/components/Home/PrintProduct.jsx
@@ -10,7 +10,7 @@ export default function PrintProduct(props) {
     <div className={props.className}>
         <img className='Img' src={hoodie.photo} />
         <div className='container-p'>
-            <p className='p'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuriesypesetting, remaining.
+            <p className='p'>Realizadas en Frisa algodón 50% y 50% poliéster, no es 100 % algodón, esto evita que encojan. Los Buzos y camperas tienen puños y cintura en Rib (Elástico). Terminación en capuchas: vienen con forrería en jersey en color a elección o corderito.
             </p>
         </div>
     </div>
